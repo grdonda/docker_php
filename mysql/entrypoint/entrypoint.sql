c31aaaa6ca59ef94323f7763a79dae2652eb1b3d
Giovanni Donda <grdonda@gmail.com>
2023-07-24 14:48:22 -0300

    backup

--- mysql/entrypoint/entrypoint.sql
+++ mysql/entrypoint/entrypoint.sql
@@ -1,2 +1,10 @@
 -- este comando SQL será executado toda vez que for iniciado o banco de dados
+
+DROP DATABASE IF EXISTS `dsiserver`;
 CREATE DATABASE IF NOT EXISTS `dsiserver` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+DROP DATABASE IF EXISTS `donda`;
+-- CREATE DATABASE IF NOT EXISTS `donda` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+DROP DATABASE IF EXISTS `dsiserver_test`;
+-- CREATE DATABASE IF NOT EXISTS `dsiserver_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
